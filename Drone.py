059d9f555ece6cc08d0c124717bfeadfdd6540b5
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-16 14:50:55 +0300

    Update and rename Drone to Drone.py

---
+++ Drone.py
@@ -0,0 +1,24 @@
+class Drone():
+    def __init__(self, Type, ID, Model, Water_Capacity, Chemical_Capacity):
+		self.Type=Type
+		self.ID=ID
+		self.Model=Model
+		self.Water_Capacity=Water_Capacity
+		self.Chemical_Capacity=Chemical_Capacity
+    
+    def CheckPrivilege(AccountPrivilege):
+		pass
+    def ReturnPrivilege(AccountPrivilege):
+		pass
+    def CheckDatabase():
+		pass
+    def AreaSelect(expanse):
+		pass
+    def ChooseOption(option):
+		pass
+    def LiveFeeding(expanse):
+		pass
+    def Spray(expanse,Chemical_Capacity):
+		pass
+    def Sprinkling(expanse,Water_Capacity):
+		pass
