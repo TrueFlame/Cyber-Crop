f96a9061865ccad7eb450147ec11c4978cecc39a
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-16 14:37:01 +0300

    Update expanse.py

--- expanse.py
+++ expanse.py
@@ -1,7 +1,8 @@
-class expanse(exp_id, size, longitude, latitude):
-    self.exp_id = exp_id
-    self.size = size
-    self. longitude = longitude
-    self.latitude = latitude 
+class expanse:
+    def __init__(self, exp_id, size, longitude, latitude):
+        self.exp_id = exp_id
+        self.size = size
+        self. longitude = longitude
+        self.latitude = latitude 
 
 area1 = expanse(0, 100, 7.9635, 8.0912)
