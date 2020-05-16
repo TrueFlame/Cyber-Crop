dced263f2b19529d4c18917fe93153c82df93ff3
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-16 14:32:17 +0300

    Class that handles Customer Support

---
+++ Customer_Support.py
@@ -0,0 +1,16 @@
+from Account.py import Account
+
+class Customer_Support:
+	
+	def show_telephone_nums():
+		telephone1= "2610765432"
+		telephone2 = "6976589302"
+		telephone3  = "210879634"
+		print(telephone1, telephone2, telephone3)
+	
+	def Choose_Category()
+		print("Choose your desired category...")
+		pass
+	
+	return SuccessCode():
+		continue
