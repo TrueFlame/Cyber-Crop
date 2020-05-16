6c5af48a9ef2db739e84b2a02b43ff862f016aeb
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-16 14:52:14 +0300

    Update and rename crop to crop.py

---
+++ crop.py
@@ -0,0 +1,9 @@
+class crop():
+    def __init__(self, crop_id, type, name, size, est_yield):
+		self.crop_id = crop_id
+		self.type = type
+		self.name = name
+		self.size = size
+		self.est_yield = est_yield
+    
+    subject_a = crop(1, tomato, 90, 1.09)
