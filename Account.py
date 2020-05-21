25ef776cfe44e2e34dfbddb1261344664fb25346
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-21 19:09:41 +0300

    Update Account.py

--- Account.py
+++ Account.py
@@ -1,30 +1,43 @@
+import tkinter
+from tkinter import ttk
+
+
 class Account:
-    def __init__(self, FirstName, LastName, Certificate, BusinessName, e-mail, password):
-    self.FirstName = FirstName
-    self.LastName = LastName
-    self.Certificate = Certificate
-    self.BusinessName = BusinessName
-    self.e-mail = e-mail
-    self.password = password
-    
-    
-    def LogIn(e-mail,password):
-    def SignUp(e-mail,passowrd,FirstName,Certificate,BusinessName):
-    def Upgrade(e-mail,passowrd,FirstName,Certificate,BusinessName):
-    
+
+    def __init__ (self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str):
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Certificate = Certificate
+        self.BusinessName = BusinessName
+        self.e_mail = e_mail
+        self.password = password
+        
+    def LogIn(e_mail,password):
+        pass
+    def SignUp(e_mail,password,FirstName,Certificate,BusinessName):
+        pass
+    def Upgrade(e_mail,password,FirstName,Certificate,BusinessName):
+        pass
     
  
  
 
 class Amateur(Account):
-    def __init__(self, e-mail, password, FirstName, LastName) : super().init(e-mail,password,FirstName,LastName)
-        account_privillege = 'Amateur' # static variable
+    def __init__(self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str): 
+        super().__init__(FirstName, LastName, Certificate, BusinessName, e_mail, password)
+        self.account_privillege = 'Amateur' # static variable
     
 class Professional(Account):
-    def __init__(self, e-mail, password, FirstName, LastName, Certificate) : super().init(e-mail,password,FirstName,LastName,Certificate)
-    account_privillege = 'Professional'
+    def __init__(self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str): 
+        super().__init__(FirstName, LastName, Certificate, BusinessName, e_mail, password)
+        self.account_privillege = 'Professional'
     
 class Business(Account):
-    def __init__(self, e-mail, password, FirstName, LastName, Certificate, BusinessName) : super().init(e-mail,password,FirstName,LastName,Certificate,BusinessName)
-                
-    account_privillege = 'Business'
+    def __init__(self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str): 
+        super().__init__(FirstName, LastName, Certificate, BusinessName, e_mail, password)
+        self.account_privillege = 'Business'
+    
+    
+    
+business1 = Business("andreoulis1996@gmail,com", "Adr5njbfas", "FirstName", "LastName", "Certificate", "BusinessName")
+print(business1.account_privillege)
