36c47d345aeb4504190244de1b9a5223541dfaea
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-21 20:49:33 +0300

    Update crop.py

--- crop.py
+++ crop.py
@@ -1,21 +1,3 @@
-class Crop:
-    def __init__ (self, crop_id: int, type: str, name: str, size: float, est_yield: str, longitude: str, latitude: str):
-        self.crop_id = crop_id
-        self.type = type
-        self.name = name
-        self.size = size
-        self.est_yield = est_yield
-        self.latitude = latitude
-        self.longitude = longitude
-        
-class Photovoltaics:
-    def __init__ (self, id, model, type, production, efficiency):
-        self.id = id
-        self.model = model
-        self.type = type
-        self.production = production
-        self.efficiency = efficiency
-
 class Expanse:
     def __init__(self, exp_id: int, size: float, longitude: float, latitude: float):
         self.exp_id = exp_id
@@ -25,11 +7,33 @@ class Expanse:
         self.crops = []
         self.photovoltaics = []
     
-    def add_crop(self, crop_ob: Crop):
+    def add_crop(self, crop_ob):
         self.crops.append({self.exp_id : crop_ob.crop_id})
 
-    def add_photovoltaic(self, photo_ob: Photovoltaics):
+    def add_photovoltaic(self, photo_ob):
         self.photovoltaics.append({self.exp_id : photo_ob.id})
+
+
+class Crop(Expanse):
+    def __init__ (self, crop_id: int, type: str, name: str, size: float, est_yield: str, longitude: str, latitude: str):
+        self.crop_id = crop_id
+        self.type = type
+        self.name = name
+        self.size = size
+        self.est_yield = est_yield
+        self.latitude = latitude
+        self.longitude = longitude
+        
+class Photovoltaics(Expanse):
+    def __init__ (self, id, model, type, production, efficiency):
+        self.id = id
+        self.model = model
+        self.type = type
+        self.production = production
+        self.efficiency = efficiency
+
+
+
 
 
 
