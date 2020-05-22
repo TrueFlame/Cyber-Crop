cada5988e1083a96e0dbe174e92f3a4249adef6c
DarthBlender <64554863+DarthBlender@users.noreply.github.com>
2020-05-22 21:56:26 +0300

    Update Greenhouse.py

--- Greenhouse.py
+++ Greenhouse.py
@@ -1,32 +1,32 @@
-class Greenhouse():
+class Greenhouse(): #Χρειάζεται εισαγωγή δεδομένων από Account και Expanse
     def __init__(self, ID, AccountPrivilege, ExpanseID, SensorID):
         self.AccountPrivilege=AccountPrivilege
         self.ExpanseID=ExpanseID
         self.ID=ID
         self.SensorID=SensorID
 		
-    def CheckPrivilege(AccountPrivilege):
+    def CheckPrivilege(AccountPrivilege): #Έλεγχος δικαιωμάτων πρόσβασης του χρήστη στην υπηρεσία
     		pass
-    def ReturnPrivilege(AccountPrivilege):
+    def ReturnPrivilege(AccountPrivilege): #Επιστροφή των δικαιωμάτων του χρήστη μέσω συνάρτησης
     		pass
-    def ChooseExpanse(ExpanseID):
+    def ChooseExpanse(ExpanseID): #Επιλογή έκτασης
     		pass
-    def ReturnErrorCode():
+    def ReturnErrorCode(): #Debug συνάρτηση
     		pass
-    def InsertData(ExpanseID):
+    def InsertData(ExpanseID): #Εισαγωγή στοιχείων αν αυτά λείπουν από την έκταση
     		pass
-    def ChooseOption():
+    def ChooseOption(): #Συνάρτηση που εμφανίζει και διαχειρίζεται τις επιλογές του χρήστη
     		pass
-    def CheckSensor(SensorID):
+    def CheckSensor(SensorID): #Έλεγχος κατάστασης των αισθητήρων
     		pass
-    def ReturnStatus(SensorID):
+    def ReturnStatus(SensorID): #Επιστροφή κατάστασης αισθητήρων
     		pass
-    def Initialize():
+    def Initialize(): #Συνάρτηση ενεργοποίησης υπηρεσίας αυτόματου ποτίσματος
     		pass
-    def DisplayStatus(ID):
+    def DisplayStatus(ID): #Προβολή εσωτερικής κατάστασης του θερμοκηπίου
     		pass
-    def EditStatus(ID):
+    def EditStatus(ID): #Διαχείριση/αλλαγή της εσωτερικής κατάστασης
     		pass
-    def AutomaticManagement(ID):
+    def AutomaticManagement(ID): #Αυτόματη διαχείριση από το σύστημα
     		pass
 
