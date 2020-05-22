ab6a62e6524b5310c54405a62ab1d30d1e7a0fab
DarthBlender <64554863+DarthBlender@users.noreply.github.com>
2020-05-22 22:01:08 +0300

    Προσθήκη σχολίων

--- History.py
+++ History.py
@@ -1,24 +1,25 @@
-class History():
+class History():#Χρειάζεται εισαγωγή δεδομένων από Account και ίσως σύνδεση με άλλες κλάσεις για αποθήκευση στοιχείων
     def __init__(self, AccountPrivilege, ExpanseID):
 		self.AccountPrivilege=AccountPrivilege
 		self.ExpanseID=ExpanseID
 		
+		#Ίσως χρειαστεί συνάρτηση που θα την χρησιμοποιούν οι άλλες κλασεις για αποθήκευση του ιστορικού
 		
-    def CheckPrivilege(AccountPrivilege):
+    def CheckPrivilege(AccountPrivilege): #Έλεγχος δικαιωμάτων πρόσβασης του χρήστη στην υπηρεσία
 		pass
-    def ReturnPrivilege(AccountPrivilege):
+    def ReturnPrivilege(AccountPrivilege): #Επιστροφή των δικαιωμάτων του χρήστη μέσω συνάρτησης
 		pass
-    def ChooseExpanse(ExpanseID):
+    def ChooseExpanse(ExpanseID): #Επιλογή έκτασης
 		pass
-    def ReturnErrorCode():
+    def ReturnErrorCode(): #Debug συνάρτηση
 		pass
-    def ChooseOption():
+    def ChooseOption(): #Συνάρτηση που εμφανίζει και διαχειρίζεται τις επιλογές του χρήστη
 		pass
-    def ViewData(ExpanseID):
+    def ViewData(ExpanseID): #Προβολή ιστορικού/πληροφοριών της συγκεκριμένης έκτασης
 		pass
-    def DeleteData(ExpanseID):
+    def DeleteData(ExpanseID): #Διαγραφή ιστορικού/πληροφοριών της συγκεκριμένης έκτασης
 		pass
-    def ExportData(ExpanseID):
+    def ExportData(ExpanseID): #Εξαγωγή των στοιχείων ιστορικού/πληροφοριών της συγκεκριμένης έκτασης σε αρχείο
 		pass
 
 
