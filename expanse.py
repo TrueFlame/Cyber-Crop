64a43e65d8b6d594a28bbfedbb09bb100c260721
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-24 17:05:48 +0300

    Update and rename expanse.py to Expanse.py

--- expanse.py
+++
@@ -1,8 +0,0 @@
-class Expanse:
-    def __init__(self, exp_id: int, size: float, longitude: float, latitude: float):
-        self.exp_id = exp_id
-        self.size = size
-        self. longitude = longitude
-        self.latitude = latitude 
-
-
