b8d5fc05c7d6c56f2d08ed7ff94d4f2881ac166e
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-24 22:56:38 +0300

    Update ProfitCalculation.py

--- ProfitCalculation.py
+++ ProfitCalculation.py
@@ -1,33 +1,31 @@
 class ProfitCalculation(): #Χρειάζεται εισαγωγή δεδομένων από Account και Expanse
     def __init__(self, AccountPrivilege, ExpanseID, CountryID, Currency, Parameters):
-		self.AccountPrivilege=AccountPrivilege
-		self.ExpanseID=ExpanseID
-		self.CountryID=CountryID
-		self.Currency=Currency
-		self.Parameters=Parameters
+        self.AccountPrivilege=AccountPrivilege
+        self.ExpanseID=ExpanseID
+        self.CountryID=CountryID
+        self.Currency=Currency
+        self.Parameters=Parameters
 		
 
     def CheckPrivilege(AccountPrivilege): #Έλεγχος δικαιωμάτων πρόσβασης του χρήστη στην υπηρεσία
-		pass
+        pass
     def ReturnPrivilege(AccountPrivilege): #Επιστροφή των δικαιωμάτων του χρήστη μέσω συνάρτησης
-		pass
+        pass
     def ChooseExpanse(ExpanseID): #Επιλογή έκτασης
-		pass
+        pass
     def ReturnErrorCode(): #Debug συνάρτηση
-		pass
+        pass
     def ChooseOption(): #Συνάρτηση που εμφανίζει και διαχειρίζεται τις επιλογές του χρήστη
-		pass
+        pass
     def ChooseParameters(CountryID,Currency): #Επιλογή χώρας πώλησης και νομίσματος 
-		pass
+        pass
     def ProfitCalculation(ExpanseID,Parameters): #Συνάρτηση υπολογισμού κέρδους
-		pass
+        pass
     def DisplayResult(): #Συνάρτηση προβολής των σχετικών πληροφοριών
-		pass
+        pass
     def DisplayError(): #Συνάρτηση προβολής σφάλματος
-		pass
+        pass
     def AutomaticCalculation(ExpanseID): #Συνάρτηση αυτόματου υπολογισμού
-		pass
+        pass
     def SaveResult(): #Συνάρτηση αποθήκευσης αποτελέσματος του υπολογισμού
-		pass
-
-
+        pass
