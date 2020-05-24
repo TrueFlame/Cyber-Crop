38c511b9d3978fabe23f73d4e7937eaad9f49573
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-24 23:36:06 +0300

    Create UserMenu.py

---
+++ UserMenu.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+class UserMenu(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+        
+        
+    def ChooseCategory():
+        pass
+    
+    def ChooseOption():
+        pass
+    
+
+root = tk.Tk()
+app = UserMenu(master=root)
+app.mainloop()
