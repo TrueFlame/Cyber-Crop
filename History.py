01021afccdb75b7820b7a590d0378b57fabdd1b4
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-24 22:53:04 +0300

    Update History.py

--- History.py
+++ History.py
@@ -1,26 +1,32 @@
+from expanse import Expanse
+
 class History():#Χρειάζεται εισαγωγή δεδομένων από Account και ίσως σύνδεση με άλλες κλάσεις για αποθήκευση στοιχείων
     def __init__(self, AccountPrivilege, ExpanseID):
-		self.AccountPrivilege=AccountPrivilege
-		self.ExpanseID=ExpanseID
-		
-		#Ίσως χρειαστεί συνάρτηση που θα την χρησιμοποιούν οι άλλες κλασεις για αποθήκευση του ιστορικού
-		
+        self.AccountPrivilege=AccountPrivilege
+        self.ExpanseID=ExpanseID
+
+#Ίσως χρειαστεί συνάρτηση που θα την χρησιμοποιούν οι άλλες κλασεις για αποθήκευση του ιστορικού
+
     def CheckPrivilege(AccountPrivilege): #Έλεγχος δικαιωμάτων πρόσβασης του χρήστη στην υπηρεσία
-		pass
+        pass
+    
     def ReturnPrivilege(AccountPrivilege): #Επιστροφή των δικαιωμάτων του χρήστη μέσω συνάρτησης
-		pass
+        pass
+    
     def ChooseExpanse(ExpanseID): #Επιλογή έκτασης
-		pass
+        pass
+    
     def ReturnErrorCode(): #Debug συνάρτηση
-		pass
+        pass
+    
     def ChooseOption(): #Συνάρτηση που εμφανίζει και διαχειρίζεται τις επιλογές του χρήστη
-		pass
+        pass
+    
     def ViewData(ExpanseID): #Προβολή ιστορικού/πληροφοριών της συγκεκριμένης έκτασης
-		pass
+        pass
+    
     def DeleteData(ExpanseID): #Διαγραφή ιστορικού/πληροφοριών της συγκεκριμένης έκτασης
-		pass
+        pass
+    
     def ExportData(ExpanseID): #Εξαγωγή των στοιχείων ιστορικού/πληροφοριών της συγκεκριμένης έκτασης σε αρχείο
-		pass
-
-
-
+        pass
