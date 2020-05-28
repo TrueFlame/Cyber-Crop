88cdd190e5ff4c795d27306e500bbc3f7627732e
TrueFlame <andreoulis1996@gmail.com>
2020-05-28 16:28:52 +0300

    Update Crop.py

--- Crop.py
+++ Crop.py
@@ -1,4 +1,4 @@
-from Expanse import Expanse
+from expanse import Expanse
 
 class Crop(Expanse):
     def __init__ (self, crop_id: int, type: str, name: str, size: float, est_yield: str, longitude: str, latitude: str):
@@ -9,3 +9,12 @@ class Crop(Expanse):
         self.est_yield = est_yield
         self.latitude = latitude
         self.longitude = longitude
+        
+
+
+crop3 = Crop(3, 'hth', "name", "size", "est_yield", "longitude", "latitude")
+
+expanse2 = Expanse(3, 8.0975, "676", "67568")
+
+expanse2.add_crop(crop3)
+print(expanse2.crops)
