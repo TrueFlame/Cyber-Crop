59db5134993c889566de507c273eaf69dc52ddc2
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-05-28 16:24:23 +0300

    Update Account.py

--- Account.py
+++ Account.py
@@ -1,8 +1,3 @@
-import tkinter
-from tkinter import ttk
-from expanse import Expanse
-from crop import Crop
-
 class Account:
 
     def __init__ (self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str):
@@ -20,13 +15,52 @@ class Account:
     def Upgrade(e_mail,password,FirstName,Certificate,BusinessName):
         pass
     
-    def ReturnPrivillege():
+    def ReturnPrivillege(self):
         pass
     def CheckPrivillege():
         pass
- 
-crop10 = Crop(4, "type", "name", 5, "est_yield", "longitude", "latitude")
+    def ChooseCategory():
+        pass
+    def CheckLogin():
+        pass
+    def CheckDatabase():
+        pass
+    
 
-print(crop10.crop_id)
+class Amateur(Account):
+    def __init__(self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str, username: str()): 
+        super().__init__(FirstName, LastName, Certificate, BusinessName, e_mail, password)
+    account_privillege = 'Amateur' # static variable
+    
+    
+class Business(Account):
+    def __init__(self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str, username : str): 
+        super().__init__(FirstName, LastName, Certificate, BusinessName, e_mail, password)
+        self.username = username
+    account_privillege = 'Business'
+    '''
+    def ReturnPrivillege(self):
+        if self.account_privillege == "Business":
+            print(True)
+    '''
+class Professional(Account):
+    def __init__(self, FirstName: str , LastName: str, Certificate: str , BusinessName: str, e_mail: str, password: str, username: str): 
+        super().__init__(FirstName, LastName, Certificate, BusinessName, e_mail, password)
+        self.username = username
+    account_privillege = 'Professional'
 
+def ReturnPrivillege(business: Business):
+        if business.account_privillege == "Business":
+            print(True)
+
+
+business1 = Business("Andrew", "oikonomou", "ok", "kati", "kati#gmail.com", "123456", "mounakias")
+
+#business1.ReturnPrivillege()
+account_dict = {}
+account_dict[business1.username] = business1.password
+
+#ReturnPrivillege(business1)
+
+print(account_dict)
 
