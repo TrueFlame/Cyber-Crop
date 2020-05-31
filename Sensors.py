8a49e8dc1451207a76ef8ebf1d7144cbc5dff62d
AuRa1996 <64554878+AuRa1996@users.noreply.github.com>
2020-05-31 20:10:03 +0300

    Update Sensors.py

--- Sensors.py
+++ Sensors.py
@@ -1,4 +1,4 @@
-class SensorSection():
+class Sensor():
     def __init__(self, Type, ID, Model, Install, Humidity, Temp, sun_exposure):
         self.Type=Type
         self.ID=ID
@@ -8,6 +8,14 @@ class SensorSection():
         self.Temp=Temp
         self.sun_exposure=sun_exposure
     
+    def CheckPrivilege(AccountPrivilege):
+        if AccountPrivilege == 'Amateur':
+            print("You don't have access to this category")
+            print("Upgrade your account Or return to user menu")
+            pass
+    def ReturnPrivilege(AccountPrivilege):
+        return AccountPrivilege
+        pass
     def CheckSensor():
         if Install == False:
             print("There is an error in Sensor Installation")	
@@ -20,6 +28,6 @@ class SensorSection():
             print("there is an error in Sensor system")
             pass
 
+  
 
-sensor = SensorSection(type,ID,Model)
-info = SensorInfo(Humidity,Temp,sun_exposure)    
+sensor = Sensor("crop", 1 ,"v2" , True, 0.7 , 30, 0.15)
