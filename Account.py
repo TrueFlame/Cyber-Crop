0af89532d3e5417b9ccd7945b4cabdd51c876604
AuRa1996 <64554878+AuRa1996@users.noreply.github.com>
2020-05-31 20:22:43 +0300

    Update Account.py

--- Account.py
+++ Account.py
@@ -64,3 +64,49 @@ account_dict[business1.username] = business1.password
 
 print(account_dict)
 
+with open('database.pkl', 'wb') as output:
+    business2 = Business("Andrew", "oikonomou", "ok", "kati", "kati#gmail.com", "123456", "kati")
+    pickle.dump(business2, output, pickle.HIGHEST_PROTOCOL)
+
+    business3 = Business("Nikos", "Papanou", "ok", "kati", "kat78678678gmail.com", "123456", "katimou")
+    pickle.dump(business3, output, pickle.HIGHEST_PROTOCOL)
+    
+    business4 = Business("Nikos", "Papanou", "okeydokey", "katitis", "kat78678678gmail.com", "123456", "katimou")
+    pickle.dump(business4, output, pickle.HIGHEST_PROTOCOL)
+
+with open('database.pkl', 'rb') as input:
+    business2 = pickle.load(input)
+    print(business1.password)  # -> banana
+    print(business1.FirstName)  # -> 40
+
+    business4 = pickle.load(input)
+    print(business4.password) # -> spam
+    print(business4.FirstName)  # -> 42
+###################################################
+'''
+def loadall(filename):
+    with open(filename, "rb") as f:
+        while True:
+            try:
+                yield dill.load(f)
+            except EOFError:
+                break
+'''
+with open("database.pkl", "rb") as data_file:
+    data = []
+    while True:
+        try:
+            data.append(pickle.load(data_file))
+        except EOFError:
+            break
+
+for obj in data:
+    obj.ReturnPrivillege()
+
+#print(items)
+
+#input_file = open("database.pkl", "rb")
+#print(dill.load(input_file))
+#ReturnPrivillege(business1)
+
+#print(account_dict)
