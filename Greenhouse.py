6b57f5d2c2122980dcc7527037e1a8f920440c68
DarthBlender <64554863+DarthBlender@users.noreply.github.com>
2020-05-31 17:02:12 +0300

    Update Greenhouse.py

--- Greenhouse.py
+++ Greenhouse.py
@@ -5,23 +5,9 @@ class Greenhouse(): #Χρειάζεται εισαγωγή δεδομένων α
         self.ID=ID
         self.SensorID=SensorID
 		
-    def CheckPrivilege(AccountPrivilege): #Έλεγχος δικαιωμάτων πρόσβασης του χρήστη στην υπηρεσία
-    		pass
-    def ReturnPrivilege(AccountPrivilege): #Επιστροφή των δικαιωμάτων του χρήστη μέσω συνάρτησης
-    		pass
-    def ChooseExpanse(ExpanseID): #Επιλογή έκτασης
-    		pass
-    def ReturnErrorCode(): #Debug συνάρτηση
-    		pass
-    def InsertData(ExpanseID): #Εισαγωγή στοιχείων αν αυτά λείπουν από την έκταση
-    		pass
     def ChooseOption(): #Συνάρτηση που εμφανίζει και διαχειρίζεται τις επιλογές του χρήστη
     		pass
-    def CheckSensor(SensorID): #Έλεγχος κατάστασης των αισθητήρων
-    		pass
     def ReturnStatus(SensorID): #Επιστροφή κατάστασης αισθητήρων
-    		pass
-    def Initialize(): #Συνάρτηση ενεργοποίησης υπηρεσίας αυτόματου ποτίσματος
     		pass
     def DisplayStatus(ID): #Προβολή εσωτερικής κατάστασης του θερμοκηπίου
     		pass
