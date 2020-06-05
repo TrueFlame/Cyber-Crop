2ecc9efe334975238e7d3187acf0129ae1f4cb72
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-06-05 21:28:29 +0300

    Update ProfitCalculation.py

--- ProfitCalculation.py
+++ ProfitCalculation.py
@@ -1,114 +1,86 @@
-from UserMenu import *
-from tkinter import *
-from PIL import Image, ImageTk
-from array import *
-
 class ProfitCalculation(): #Χρειάζεται εισαγωγή δεδομένων από Account και Expanse
-    def __init__(self, CountryID, CurrencyID, Parameters):
-        self.CountryID=CountryID
-        self.Currency=CurrencyID
-        self.Parameters=Parameters
+    def __init__(self, ExpanseID, CountryID, CurrencyID):
+        self.ExpanseID=ExpanseID
+        self.CountryID = CountryID
+        self.CurrencyID = CurrencyID
 		
-        Country=[
-                "USA",
-                "Germany",
-                "France",
-                "Greece",
-                "Russia",
-                "China"
-                ]
+
+    def prof_calc_crop(self, crop_ob): #Συνάρτηση υπολογισμού κέρδους
+        transport_cost = 100 * crop_ob.est_yield
+        selling_income = 1500 * crop_ob.est_yield
+        true_profit = selling_income - transport_cost
         
-        Currency=[
-                "EUR",
-                "USD",
-                "CNY"
-                ]
+        if self.CountryID == "USA":
+            profit_usd = true_profit*1.3
+            print("True profit in America is: "+profit_usd +" " +self.CurrencyID)
         
-        #dummy dedomena-------------------------------------------
-        CropSale1=[[0, 200], [1,100], [2, 50], [3,60]]#country that buys + price per ton
-        CropSale2=[[3, 20], [2, 50], [1,60]]
-        CropSale3=[[3, 20], [1,10], [2, 5], [3,6]]
+        elif self.CountryID == "Germany":
+            german_transport_cost = transport_cost*2.0
+            new_profit = selling_income - german_transport_cost
+            print("True profit in Germany is: "+ new_profit +" " +self.CurrencyID)
         
+        elif self.CountryID == "France":
+            print("True profit in France is: "+ true_profit +" "+ self.CurrencyID)
         
-    def ChooseOption(): #Συνάρτηση διαχειρίζεται τις επιλογές του χρήστη apo gui
-        '''
-        # logo apo user menu class
-        photo = ImageTk.PhotoImage(Image.open("logo.png"))
-        label1 = tk.Label(self, image = photo)
-        label1.image = photo # εχει προβληματα με το garbage disposal και γιαυτο κραταμε μια αναφορα στην εικόνα
-        label1.grid(row = 0, column = 1, columnspan = 6)
-        '''
+        elif self.CountryID == "England":
+            profit_eng = true_profit*0.8
+            print("True profit in England is: "+ profit_eng+ " "+ self.CurrencyID)
         
-        CalcWin= Tk()
-        CalcWin.title("Profit Calculation")
-        CalcWin.iconbitmap("Logo.png")
-        CalcWin.geometry("640x480")
-       
-        #Drop Down Box
-        options=[
-                "Profit Calculation", 
-                "Automatic Calculation"
-                ]      
-        click = StringVar()
-        click.set(options[0])
+        elif self.CountryID == "Greece":
+            print("True profit in Greece is: "+ true_profit+" "+ self.CurrencyID)
         
-        drop= OptionMenu(CalcWin, click, *options)
-        drop.pack()
-        pass
-   
-    
-    def ChooseParameters(CountryID,CurrencyID): #Επιλογή χώρας πώλησης και νομίσματος apo gui
-        ChosenCount=index("CountryID") #metatroph sto antistoixo id ths listas
-        ChosenCurr=index("CurrencyID")
-        Parameters=[ChosenCount,ChosenCurr]
-        pass
-   
-    
-    def ProfitCalculation(ExpanseID,Parameters): #Συνάρτηση υπολογισμού κέρδους
-        found = false
-        price = -1
+        elif self.CountryID == "Russia":
+            new_profit = true_profit*77.0
+            print("True profit in Russia is: "+ new_profit+" "+ self.CurrencyID)
         
-        for r in CropSale1:
-               if Parameters[1]==r:
-                   price=CropSale1[r,2]
-                   found =true
-        if found == true:
-            DisplayResult(CountryID,price)
-        else:DisplayError(found)
-        pass
-   
-    
-    def DisplayResult(): #Συνάρτηση προβολής των σχετικών πληροφοριών
-        pass
-    
-    
-    def DisplayError(param): #Συνάρτηση προβολής σφάλματος
-        if param==false:
-            print("No country buys this crop")
-        pass
-    
+        elif self.CountryID == "China":
+            new_profit = true_profit*8.0
+            print("True profit in China is: "+ new_profit+" "+ self.CurrencyID)
+
+
+    def prof_calc_photovoltaic(self, photo_ob):
+        #only selling within country of origin possible
+        selling_income = 100 * photo_ob.production* photo_ob.efficiency
+        
+        if self.CountryID == "USA":
+            profit_usd = selling_income * 0.7
+            print("True profit in America is: "+profit_usd +" " +self.CurrencyID)
+        
+        elif self.CountryID == "Germany":
+            new_profit = selling_income * 2.0
+            print("True profit in Germany is: "+ new_profit +" " +self.CurrencyID)
+        
+        elif self.CountryID == "France":
+            new_profit = selling_income * 1.5
+            print("True profit in France is: "+ new_profit +" "+ self.CurrencyID)
+        
+        elif self.CountryID == "England":
+            profit_eng = selling_income * 2.0
+            print("True profit in England is: "+ profit_eng+ " "+ self.CurrencyID)
+        
+        elif self.CountryID == "Greece":
+            print("True profit in Greece is: "+ selling_income +" "+ self.CurrencyID)
+        
+        elif self.CountryID == "Russia":
+            new_profit = selling_income * 77.0
+            print("True profit in Russia is: "+ new_profit+" "+ self.CurrencyID)
+        
+        elif self.CountryID == "China":
+            new_profit = selling_income * 8.0
+            print("True profit in China is: "+ new_profit+" "+ self.CurrencyID)
     
     def AutomaticCalculation(ExpanseID): #Συνάρτηση αυτόματου υπολογισμού
-        max=-1
-        location=-1
-        locationiso=-1
-        iso=false
-        for r in CropSale1:
-            for c in r:
-                if CropSale1[r,c]>max:
-                    max=CropSale1[r,c]
-                    location=r
-                    
-                elif CropSale1[r,c]==max: #se periptwsh pou dyo xwres exoun thn idia timh pwlhshs, epistrefei kai tis dyo.
-                    locationiso=r
-                    iso=true
-           
-            #xrhsh ths display synarthshs mesw gui element
-            print(Country[location],max)
-            if iso==true:
-                print(Country[locationiso],max)
         pass
-   
     
     def SaveResult(): #Συνάρτηση αποθήκευσης αποτελέσματος του υπολογισμού
         pass
+
+    
+def ChooseParameters(): #Επιλογή χώρας πώλησης και νομίσματος    
+    country_and_currency = {"USA" : "USD", "Germany" : "EUR", "France": "EUR", "England" : "GBP", "Greece" : "EUR", "Russia" : "RUB", "China" : "CNY"}    
+
+    print("The available countries are USA, Germany, France, England, Greece, Russia, China...")
+    country = input("Select on of the above. \n")
+    currency = country_and_currency.get(country)
+    return country, currency
+
