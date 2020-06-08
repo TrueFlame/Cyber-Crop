64f3c386beff251db614b950a7a352975e07abdd
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-06-08 20:40:03 +0300

    Update Sensor.py

--- Sensor.py
+++ Sensor.py
@@ -13,10 +13,10 @@ channel = 21 # for nonexistent arduino sensor
 #GPIO.setup(channel, GPIO.IN)
  
 class Sensor():
-    def __init__(self, Type, ID, Model, Installation = None):
-        self.Type=Type
-        self.ID=ID
-        self.Model=Model
+    def __init__(self, area_id, Type, Model, Installation = None):
+        self.area_id = area_id
+        self.Type = Type ### type is either crop or photovoltaics
+        self.Model = Model
         self.Installation = self.diagnosis()
         self.Humidity, self.Temp, self.sun_exposure = self.get_data()
 
@@ -40,12 +40,11 @@ class Sensor():
         temperature = round(random.uniform(5.0, 40.0), 2)
         sun_exposure = round(random.randint(300, 1000), 2)
         return humidity, temperature, sun_exposure
+    
         
     def diagnosis(self):
         if channel != 21:
-            print (" Nothing is running")
             return False
         else:
-            print ("Sensor is working!")
             return True
     
