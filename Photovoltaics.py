79dd9e3c58326bbe66c0ae6c9863f7cf9ab52a47
TrueFlame <andoikonomou@ceid.upatras.gr>
2020-06-08 21:52:05 +0300

    Delete Photovoltaics.py

--- Photovoltaics.py
+++
@@ -1,9 +0,0 @@
-from Expanse import Expanse
-
-class Photovoltaics(Expanse):
-    def __init__ (self, id, model, type, production, efficiency):
-        self.id = id
-        self.model = model
-        self.type = type
-        self.production = production
-        self.efficiency = efficiency
